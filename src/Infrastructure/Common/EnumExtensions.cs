66337b210255398a63ef8e718a529aa4783d4421
kirill.mikhailiuk <kirill.mikhailiuk@FNSWIN25>
2025-03-12 17:55:14 +0600

    енамка для заголовков уведомлений

---
+++ src/Infrastructure/Common/EnumExtensions.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel;
+
+namespace Common;
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        if (field == null) return value.ToString();
+
+        var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+        return attribute?.Description ?? value.ToString();
+    }
+}
